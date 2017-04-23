679f6480f9c9c9e7506ac994d74317c34a890462
Vitaly Chipounov <vitaly@cyberhaven.io>
2017-04-23 16:07:25 +0200

    make-kernel.sh: prevent the script from being ran as root
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.io>

--- Linux/docker/make-kernel.sh
+++ Linux/docker/make-kernel.sh
@@ -31,6 +31,25 @@ fi
 MUID=$2
 MGID=$3
 
+if [ $MUID -eq 0 -o $MGID -eq 0 ]; then
+    echo "Do not run this script as root"
+    exit 1
+fi
+
+# Verify that the specified group and user ids don't exist locally.
+# If so, delete them. This may happen if the host OS is not Debian-based,
+# where user ids may conflict with those preinstalled in the docker image.
+GROUP=$(getent group $MGID | cut -d ':' -f 1)
+USER=$(getent passwd $MUID | cut -d ':' -f 1)
+
+if [ "x$USER" != "x" ]; then
+  userdel $USER
+fi
+
+if [ "x$GROUP" != "x" ]; then
+  groupdel $GROUP
+fi
+
 groupadd -g $MGID s2e
 useradd -u $MUID -g s2e s2e
 
