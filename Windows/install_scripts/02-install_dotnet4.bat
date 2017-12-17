5bcff5776fff8e517d1018429289ff9e99c0c1bf
Vitaly Chipounov <vitaly@cyberhaven.io>
2017-12-17 19:26:42 +0100

    windows: install .net 4.5 for win7 and above
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.io>

--- Windows/install_scripts/02-install_dotnet4.bat
+++ Windows/install_scripts/02-install_dotnet4.bat
@@ -1,7 +1,16 @@
 setlocal EnableDelayedExpansion EnableExtensions
 
-echo ==^> Installing .NET 4.0
+echo ==^> Installing .net
 
+:: Windows XP does not support versions above 4.0
+ver | find "5.1" > nul
+if not %ERRORLEVEL% == 0 goto notxp
+echo Installing .net 4.0
 %~dp0\inst\dotNetFx40_Full_x86_x64.exe /Q /norestart
+goto end
 
+:notxp
+echo Installing .net 4.5
+%~dp0\inst\dotNetFx45_Full_x86_x64.exe /Q /norestart
 
+:end
