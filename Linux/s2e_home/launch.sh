b5e91b44abcab6aeecd96fef7144be65219831f5
Vitaly Chipounov <vitaly@cyberhaven.com>
2022-07-17 21:53:07 +0200

    linux: don't install kernel libc packages
    This breaks apt-get
    
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.com>

--- Linux/s2e_home/launch.sh
+++ Linux/s2e_home/launch.sh
@@ -107,7 +107,7 @@ install_systemtap() {
 # Install kernels last, the cause downgrade of libc,
 # which will cause issues when installing other packages
 install_kernel() {
-    sudo dpkg -i *.deb
+    sudo dpkg -i linux-image*.deb linux-headers*.deb
 
     MENU_ENTRY="$(grep menuentry /boot/grub/grub.cfg  | grep s2e | cut -d "'" -f 2 | head -n 1)"
     echo "Default menu entry: $MENU_ENTRY"
